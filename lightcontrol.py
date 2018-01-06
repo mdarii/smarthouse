d747325ce489b604b5280817ca43b4e74c638525
Maxim Darii <dariimaxim@gmail.com>
2018-01-06 23:39:36 +0200

    Added error handling for Mqtt connection

--- lightcontrol.py
+++ lightcontrol.py
@@ -5,10 +5,12 @@ import time
 import os
 from sys import exit
 import gc
-from umqtt.robust import MQTTClient
+from umqtt.simple import MQTTClient
 
 gc.enable()
 gc.collect()
+
+alarm_led = Pin(10, mode=Pin.OUT)
 
 def check_file(file):
     try:
@@ -37,6 +39,23 @@ def sub_cb(topic, msg):
     print(topic)
     print(msg)
 
+def mqtt_conect(server,port,client_id,user,password,alarm_led,topics):
+    print('Connecting to Mqtt server')
+    mqttclient = MQTTClient(client_id,server=server,port=port,user=user,password=password)
+    mqttclient.set_callback(sub_cb)
+    try:
+        mqttclient.connect()
+        time.sleep_ms(10)
+        alarm_led.off()
+        for topic in topics:
+            mqttclient.subscribe(topic=topic)
+        print("Mqqt connected")
+        return mqttclient, False
+    except:
+        alarm_led.on()
+        print("Mqqt isn't connected, retry in 60 seconds ")
+        return False, time.time()
+
 if check_file('config.json'):
     config=ujson.loads(read_file('config.json'))
 else:
@@ -51,12 +70,16 @@ if config['wifi']:
     do_connect(sta_if,config['wifi'])
 
 if config['mqtt']:
-    mqttclient = MQTTClient(config['mqtt']['client_id'], server=config['mqtt']['server'], port=config['mqtt']['port'], user=config['mqtt']['user'], password=config['mqtt']['password'])
-    mqttclient.set_callback(sub_cb)
-    mqttclient.DEBUG = True
-    mqttclient.connect()
-    for topic in config['mqtt']['topics']:
-        mqttclient.subscribe(topic=topic)
+        mqtt_client, last_try = mqtt_conect(config['mqtt']['server'],config['mqtt']['port'],config['mqtt']['client_id'],config['mqtt']['user'],config['mqtt']['password'],alarm_led,config['mqtt']['topics'])
 
-print('Check for messages')
-mqttclient.check_msg()
+while True:
+    if mqtt_client:
+        print('Check for messages')
+        try:
+            mqtt_client.check_msg()
+        except:
+            mqtt_client = False
+            alarm_led.on()
+    elif time.time() >= (last_try+60):
+        mqtt_client, last_try = mqtt_conect(config['mqtt']['server'],config['mqtt']['port'],config['mqtt']['client_id'],config['mqtt']['user'],config['mqtt']['password'],alarm_led,config['mqtt']['topics'])
+    time.sleep_ms(100)
